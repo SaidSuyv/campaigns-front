71a2cd364911e0460b15dcb7a492012a0d06f679
SaidSuyv <sirsuyvi@gmail.com>
2025-03-03 11:10:24 -0500

    init

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { InputComponent } from '../../components/input/input.component';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    InputComponent
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
